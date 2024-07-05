72b1070e84f014277d4fd2acadffb44e1baeafb4
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-07-05 07:13:19 +0200

    migrations applied

--- project_management/tasks/serializers.py
+++ project_management/tasks/serializers.py
@@ -10,38 +10,30 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined']
 
 class ProjectSerializer(serializers.ModelSerializer):
-    owner = UserSerializer(read_only=True)
 
     class Meta:
         model = Project
         fields = ['id', 'name', 'description', 'owner', 'created_at', 'updated_at']
 
 class MilestoneSerializer(serializers.ModelSerializer):
-    project = ProjectSerializer(read_only=True)
 
     class Meta:
         model = Milestone
         fields = ['id', 'name', 'due_date', 'project', 'created_at', 'updated_at']
 
 class TaskSerializer(serializers.ModelSerializer):
-    milestone = MilestoneSerializer(read_only=True)
-    assignee = UserSerializer(read_only=True)
 
     class Meta:
         model = Task
         fields = ['id', 'name', 'description', 'milestone', 'assignee', 'completed', 'created_at', 'updated_at']
 
 class TaskCommentSerializer(serializers.ModelSerializer):
-    task = TaskSerializer(read_only=True)
-    user = UserSerializer(read_only=True)
 
     class Meta:
         model = TaskComment
         fields = ['id', 'task', 'user', 'comment', 'created_at']
 
 class TaskLogSerializer(serializers.ModelSerializer):
-    task = TaskSerializer(read_only=True)
-    user = UserSerializer(read_only=True)
 
     class Meta:
         model = TaskLog
