58b6f3f8edd56df6cb2e2efa7de06cbd22db6105
YENG-SEBASTIAN <yengsebastian1@gmail.com>
2024-07-05 06:46:30 +0200

    taks views

--- project_management/tasks/views.py
+++ project_management/tasks/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+# views.py
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.core.mail import send_mail
+from tasks.models import Project, Milestone, Task, TaskComment
+from tasks.serializers import ProjectSerializer, MilestoneSerializer, TaskSerializer, TaskCommentSerializer
+from tasks.permissions import IsProjectOwner, IsAssignee
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, IsProjectOwner]
+
+class MilestoneListCreateView(generics.ListCreateAPIView):
+    queryset = Milestone.objects.all()
+    serializer_class = MilestoneSerializer
+    permission_classes = [IsAuthenticated]
+
+class MilestoneDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Milestone.objects.all()
+    serializer_class = MilestoneSerializer
+    permission_classes = [IsAuthenticated, IsProjectOwner]
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        task = serializer.save()
+        if task.assignee:
+            send_mail(
+                'You have been assigned a new task',
+                f'You have been assigned to the task: {task.name}',
+                'from@example.com',
+                [task.assignee.email],
+                fail_silently=False,
+            )
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated, IsProjectOwner | IsAssignee]
+
+class TaskCommentListCreateView(generics.ListCreateAPIView):
+    queryset = TaskComment.objects.all()
+    serializer_class = TaskCommentSerializer
+    permission_classes = [IsAuthenticated]
+
+class TaskCommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TaskComment.objects.all()
+    serializer_class = TaskCommentSerializer
+    permission_classes = [IsAuthenticated, IsProjectOwner | IsAssignee]
