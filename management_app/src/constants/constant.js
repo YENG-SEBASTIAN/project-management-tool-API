9185ec98f89f42f4991704a395a97c4f0952877e
Yeng Sebastian <60155150+YENG-SEBASTIAN@users.noreply.github.com>
2024-07-19 12:05:54 +0000

    Update constant.js

--- management_app/src/constants/constant.js
+++ management_app/src/constants/constant.js
@@ -1,4 +1,4 @@
 
 // export const base_url = 'http://localhost:8000/'
 
-export const base_url = 'https://yengsabs.pythonanywhere.com/api/'
+export const base_url = 'https://yengsabs.pythonanywhere.com/'
